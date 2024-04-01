06a0a77080a1bceb75e578022c50a6102bbe01fc
vivek <vivek@gmail.com>
2024-04-01 14:45:07 +0530

    index blog titles set

--- resources/views/website/pages/blogs.blade.php
+++ resources/views/website/pages/blogs.blade.php
@@ -12,20 +12,20 @@
     </div>
     <div class="row justify-content-center blog-active">
       <?php foreach ($frontblogarticlelist as $key => $frontblogarticlelist_data) {?>
-      <div class="col-lg-3 col-md-6 card1">
-      <div class="card-deck">
-        <div class="card">
-          <img src="<?php echo  $frontblogarticlelist_data['photopath']; ?>" class="card-img-top" alt="...">
-          <div class="card-body">
-          <h4><a><?php echo $frontblogarticlelist_data['title']; ?></a></h4>
-            
-          </div>
-          <div class="card-footer">
-            <a href="{{ route('sub-blogs', ['id' => $frontblogarticlelist_data['id']]) }}" class="arrow-btn">Read More
-              <span></span></a>
+      <div class="col-lg-4 col-md-6 card1">
+        <div class="card-deck">
+          <div class="card">
+            <img src="<?php echo  $frontblogarticlelist_data['photopath']; ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h4><a><?php echo $frontblogarticlelist_data['title']; ?></a></h4>
+              
+            </div>
+            <div class="card-footer">
+              <a href="{{ route('sub-blogs', ['id' => $frontblogarticlelist_data['id']]) }}" class="arrow-btn">Read More
+                <span></span></a>
+            </div>
           </div>
         </div>
-      </div>
       </div>
 
 
