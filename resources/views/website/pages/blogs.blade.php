87e5861e4b11847f0c97750d73c4afc31db8b89c
vivek <vivek@gmail.com>
2024-04-01 14:51:18 +0530

    index blog titles set

--- resources/views/website/pages/blogs.blade.php
+++ resources/views/website/pages/blogs.blade.php
@@ -18,7 +18,8 @@
             <a><img src="<?php echo  $frontblogarticlelist_data['photopath']; ?>" alt=""></a>
           </div>
           <div class="blog-post-content">
-            <h4><a><?php echo $frontblogarticlelist_data['title']; ?></a></h4>
+          <h4><a><?php echo (mb_strlen($frontblogarticlelist_data['title'], 'UTF-8') > 20) ? mb_substr($frontblogarticlelist_data['title'], 0, 20, 'UTF-8') . '...' : $frontblogarticlelist_data['title']; ?></a></h4>
+
             <!-- <p><?php echo $frontblogarticlelist_data['content']; ?></p> -->
             <a href="{{ route('sub-blogs', ['id' => $frontblogarticlelist_data['id']]) }}" class="arrow-btn">Read More
               <span></span></a>
